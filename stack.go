2448704014c40e053086f64b01bbd84a0768ed88
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-06-13 22:23:38 +0530

    improve perf

--- stack.go
+++ stack.go
@@ -25,17 +25,16 @@ func NewStack() *Stack {
 
 // Pop pops value from the top of the stack.
 func (s *Stack) Pop() (value unsafe.Pointer) {
-	// var top, next unsafe.Pointer
+	var top, next unsafe.Pointer
 	for {
-		top := atomic.LoadPointer(&s.top)
+		top = atomic.LoadPointer(&s.top)
 		if top == nil {
 			return
 		}
-		next := atomic.LoadPointer(&(*directItem)(top).next)
+		next = atomic.LoadPointer(&(*directItem)(top).next)
 		if atomic.CompareAndSwapPointer(&s.top, top, next) {
 			value = (*directItem)(top).value
 			(*directItem)(top).next, (*directItem)(top).value = nil, nil
-			// item.next, item.value = nil, nil
 			itemPool.Put((*directItem)(top))
 			return
 		}
@@ -44,10 +43,11 @@ func (s *Stack) Pop() (value unsafe.Pointer) {
 
 // Push pushes a value on top of the stack.
 func (s *Stack) Push(v unsafe.Pointer) {
-	item := itemPool.Get().(*directItem)
+	var (
+		top  unsafe.Pointer
+		item = itemPool.Get().(*directItem)
+	)
 	item.value = v
-	// item.next, item.value = nil, v
-	var top unsafe.Pointer
 	for {
 		top = atomic.LoadPointer(&s.top)
 		item.next = top
