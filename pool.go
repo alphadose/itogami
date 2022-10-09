4c542897737a6dd4c40bbf8628b837dc79cceca1
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-10-09 08:20:44 +0530

    remove unnecessary code

--- pool.go
+++ pool.go
@@ -18,37 +18,23 @@ func NewPool(size uint64) *Pool {
 }
 
 // Submit submits a new task to the pool
-// it first tries to use already parked goroutines from the stack if any
-// if there are no available worker goroutines, it tries to add a
-// new goroutine to the pool if the pool capacity is not exceeded
-// in case the pool capacity hit its maximum limit, this function yields the processor to other
-// goroutines and loops again for finding available workers
+// tries to re-use existing goroutine if available else it spawns a new goroutine
 func (p *Pool) Submit(task func()) {
-	var s *node
-	for {
-		if s = p.pop(); s != nil {
-			s.task = task
-			safe_ready(s.threadPtr)
-			return
-		} else {
-			s = &node{task: task}
-			go p.loopQ(s)
-			return
-		}
+	if s := p.pop(); s != nil {
+		s.task = task           // assign task to existing worker goroutine
+		safe_ready(s.threadPtr) // start the goroutine
+	} else {
+		go p.loopQ(task) // spawn new worker goroutine
 	}
 }
 
 // loopQ is the looping function for every worker goroutine
-func (p *Pool) loopQ(s *node) {
-	// store self goroutine pointer
-	s.threadPtr = GetG()
+func (p *Pool) loopQ(task func()) {
+	state := &node{threadPtr: GetG(), task: task}
 	for {
-		// exec task
-		s.task()
-		// notify availability by pushing self reference into stack
-		p.push(s)
-		// park and wait for call
-		mcall(fast_park)
+		state.task()     // exec task
+		p.push(state)    // notify availability by pushing state reference into stack
+		mcall(fast_park) // park and wait for call
 	}
 }
 
